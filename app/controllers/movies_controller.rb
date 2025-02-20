9f9d33f66f5c37280c6b5f78e19818967c368499
yelmasry <yarra.elmasry@gmail.com>
2025-02-20 22:22:12 +0000

    stuck

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,24 @@
 class MoviesController < ApplicationController
+  def update 
+    m_id = params.fetch("the_id")
+
+    matching_records = Movie.where({ :id => m_id})
+    the_movie = matching_records.at(0) 
+
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+    the_movie.title = params.fetch("the_title")
+
+    the_movie.save 
+
+    redirect_to("/movies/#{the_movie.id}")
+  end 
+
+
+
   def create
     m = Movie.new
     m.title = params.fetch("the_title")
@@ -15,6 +35,19 @@ class MoviesController < ApplicationController
   end 
   
   
+  def destroy  
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id})
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy 
+
+    redirect_to("/movies")
+  end 
+
+
   
   def index
     matching_movies = Movie.all
